efb28fb1bcf64c00e96f0bf167ef6550bec9195d
JanKaram2020 <65977048+JanKaram2020@users.noreply.github.com>
2021-07-31 17:49:33 +0200

    removed unused import from layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Navigation from './Navigation';
 import Footer from './Footer';
 
