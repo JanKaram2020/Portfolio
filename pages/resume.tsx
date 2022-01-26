b50893b4514977b0f375b77fe0e5be2ad577e11f
Jan <jankaram2020@gmail.com>
2022-01-27 01:36:12 +0200

    update resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -7,15 +7,16 @@ import {
   VStack,
   Divider,
   Center,
-  Box,
 } from '@chakra-ui/react';
+import { MdEmail, MdPhone } from 'react-icons/md';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const TwoColumnGrid = ({ children }: { children: React.ReactNode }) => (
   <SimpleGrid
-    columns={{ sm: 1, lg: 2 }}
+    columns={{ sm: 1 }}
     sx={{
       '@media print': {
-        gridTemplateColumns: '1fr 1fr',
+        gridTemplateColumns: '1fr',
       },
     }}
     w="100%"
@@ -44,7 +45,7 @@ export default function ResumePage() {
         >
           <Link
             href="/Resume.pdf"
-            download="JanKaram-Resume.pdf"
+            download="Jan-Karam-Resume.pdf"
             textAlign="center"
             fontSize="2xl"
             fontWeight="bold"
@@ -52,12 +53,13 @@ export default function ResumePage() {
           >
             Download Resume
           </Link>
-          or print this page and all extras will be removed from print
+          or print this page and all extras will be removed from print <br />
+          (remember to disable headers and footers)
         </Text>
       </Center>
       <Center>
         <VStack
-          spacing={5}
+          spacing={2}
           divider={<Divider />}
           maxW="700px"
           mt="5vh"
@@ -67,62 +69,103 @@ export default function ResumePage() {
           sx={{
             '@media print': {
               m: 0,
+              p: 0,
               minWidth: '100%',
+              border: 'none',
             },
           }}
         >
+          <SimpleGrid>
+            <Heading
+              as="h1"
+              textAlign="center"
+              fontSize="2xl"
+              display="none"
+              sx={{
+                '@media print': {
+                  display: 'block',
+                },
+              }}
+            >
+              Jan Karam Aziz Ghaly
+            </Heading>
+            <Heading
+              as="h2"
+              textAlign="center"
+              fontSize="2xl"
+              display="block"
+              sx={{
+                '@media print': {
+                  display: 'none',
+                },
+              }}
+            >
+              Contact
+            </Heading>
+            <SimpleGrid columns={2} gap="5px">
+              <Center>
+                <MdEmail />
+                <Link href="mailto: jankaram2020@gmail.com" external>
+                  jankaram2020@gmail.com
+                </Link>
+              </Center>
+              <Center>
+                <MdPhone />
+                <Link href="tel: +201277440641" external>
+                  (+2) 012 77 44 0641{' '}
+                </Link>
+              </Center>
+              <Center>
+                <AiFillLinkedin />
+                <Link href="https://www.linkedin.com/in/jankaram2020/" external>
+                  LinkedIn
+                </Link>
+              </Center>
+              <Center>
+                <AiFillGithub />
+                <Link href="https://github.com/JanKaram2020" external>
+                  Github
+                </Link>
+              </Center>
+            </SimpleGrid>
+          </SimpleGrid>
           <TwoColumnGrid>
-            <Box>
-              <Heading
-                as="h1"
-                fontSize="2xl"
-                display="none"
-                sx={{
-                  '@media print': {
-                    display: 'block',
-                  },
-                }}
-              >
-                Jan Karam Aziz Ghaly
-              </Heading>
-              <Heading as="h2" fontSize="xl">
-                Contact
-              </Heading>
-            </Box>
-            <Text>
-              <Link href="mailto: jankaram2020@gmail.com" external>
-                jankaram2020@gmail.com
-              </Link>
-              <br />
-              <Link href="tel: +201277440641" external>
-                (+2) 012 77 44 0641{' '}
-              </Link>
-              <br />
-              <Link href="https://www.linkedin.com/in/jankaram2020/" external>
-                Linked in
-              </Link>
-              <br />
-              <Link href="https://github.com/JanKaram2020" external>
-                Github
-              </Link>
-            </Text>
-          </TwoColumnGrid>
-          <TwoColumnGrid>
-            <Heading as="h2" fontSize="xl" id="aboutMe">
+            <Heading as="h2" fontSize="xl" id="aboutMe" textAlign="center">
               Professional Summary
             </Heading>
-            <Text>
-              Meticulous web developer trained in HTML5, CSS3, Javascript and
-              React js with over 2 years of frontend experience and passion for
-              responsive website design and a firm believer in the mobile-first
-              approach.
+            <Text fontSize="14px">
+              Meticulous full stack web developer trained in React js, Node js,
+              and Express with over 2 years of experience and passion for
+              creating web applications.
             </Text>
           </TwoColumnGrid>
           <TwoColumnGrid>
-            <Heading as="h2" fontSize="xl">
+            <Heading as="h2" fontSize="xl" textAlign="center">
               Experience
             </Heading>
             <Text>
+              <Text>
+                <Heading as="h3" fontSize="lg">
+                  Full stack web developer
+                </Heading>
+                Cool code marketing agency, Remote
+                <Text fontSize="sm">Aug 2021 - Nov 2021</Text>
+                <Text as="ol" ml="1rem" fontSize="14px">
+                  <li>
+                    Implemented an app interface for a customer-facing Medical
+                    application using react, typescript, and Material Ui
+                  </li>
+                  <li>
+                    developed more than 20+ components reusable components used
+                    throughout the app.
+                  </li>
+                  <li>
+                    Implemented API endpoints and relations between models using
+                    Node, Express, and Sequelize.
+                  </li>
+                </Text>
+                <Divider />
+              </Text>
               <Text>
                 <Heading as="h3" fontSize="lg">
                   Frontend Developer
@@ -136,21 +179,16 @@ export default function ResumePage() {
                   Internship Trainee
                 </Heading>
                 Information Technology Institute (ITI)
-                <Text fontSize="sm">Jul 2019 - Aug 2019 (2 months)</Text>
+                <Text fontSize="14px">Jul 2019 - Aug 2019 (2 months)</Text>
                 attended open source development (120 hours) including:-
-                <Text as="ol" ml="1rem">
-                  <li>Web development basics with HTML and CSS (12 hours)</li>
-                  <li>JavaScript programming (24 hours)</li>
-                  <li>MySQL (18 hours)</li>
-                  <li>Apache Web Server (6 hours)</li>
-                  <li>PHP programming language (30 hours) </li>
-                  <li>Laravel framework (30 hours)</li>
+                <Text as="ol" ml="1rem" fontSize="14px">
+                  HTML - CSS - Javascript - MySQL - Php - Laravel
                 </Text>
               </Text>
             </Text>
           </TwoColumnGrid>
           <TwoColumnGrid>
-            <Heading as="h2" fontSize="xl" id="projects">
+            <Heading as="h2" fontSize="xl" id="projects" textAlign="center">
               Projects
             </Heading>
             <Text>
@@ -166,27 +204,11 @@ export default function ResumePage() {
                   https://lilyannehany.netlify.app/
                 </Link>
               </Text>
-              <Text>
-                <Heading as="h3" fontSize="md">
-                  Stack
-                </Heading>
-                <Text as="ol" ml="1rem">
-                  <li>Gatsby Js and React for Ui development.</li>
-                  <li>i18next for internalization.</li>
-                  <li>Theme Ui for light & dark mode and styling.</li>
-                  <li>Sanity CMS for content management.</li>
-                </Text>
-              </Text>
-              <Text>
-                <Heading as="h3" fontSize="md">
-                  Lighthouse score
-                </Heading>
-                <Text as="ol" ml="1rem">
-                  <li>100/100 Performance.</li>
-                  <li>97/100 Accessibility.</li>
-                  <li>100/100 Best Practices.</li>
-                  <li>100/100 SEO.</li>
-                </Text>
+              <Text fontSize="14px">
+                Made it using React for UI development, Gatsby for SSG and
+                Graphql for data fetching, i18next for internalization, Theme Ui
+                for light & dark mode and styling, and Sanity CMS for content
+                management.
               </Text>
               <Divider />
               <Text>
@@ -201,68 +223,16 @@ export default function ResumePage() {
                   https://jan-would-you-rather.surge.sh
                 </Link>
               </Text>
-              <Text>
-                <Heading as="h3" fontSize="md">
-                  Stack
-                </Heading>
-                <Text as="ol" ml="1rem">
-                  <li>React for Ui development.</li>
-                  <li>Redux and Redux-toolkit for state management.</li>
-                  <li>Bootstrap and React-strap for styling.</li>
-                  <li>JSON file as a database.</li>
-                </Text>
-              </Text>
-              <Text>
-                <Heading as="h3" fontSize="md">
-                  Lighthouse score
-                </Heading>
-                <Text as="ol" ml="1rem">
-                  <li>93/100 Performance.</li>
-                  <li>96/100 Accessibility.</li>
-                  <li>100/100 Best Practices.</li>
-                  <li>91/100 SEO.</li>
-                </Text>
-              </Text>
-              <Divider />
-              <Text>
-                <Heading as="h3" fontSize="lg">
-                  Music website
-                </Heading>
-                <Link
-                  isExternal
-                  rel="noopener"
-                  href="https://eargasm.surge.sh/"
-                >
-                  https://eargasm.surge.sh/
-                </Link>
-              </Text>
-              <Text>
-                <Heading as="h3" fontSize="md">
-                  Stack
-                </Heading>
-                <Text as="ol" ml="1rem">
-                  <li>React for Ui development.</li>
-                  <li>Bootstrap and React-strap for styling.</li>
-                  <li>JSON file as a database.</li>
-                  <li>Fuse.js for search engine.</li>
-                </Text>
-              </Text>
-              <Text>
-                <Heading as="h3" fontSize="md">
-                  Lighthouse score
-                </Heading>
-                <Text as="ol" ml="1rem">
-                  <li>93/100 Performance.</li>
-                  <li>97/100 Accessibility.</li>
-                  <li>93/100 Best Practices.</li>
-                  <li>90/100 SEO.</li>
-                </Text>
+              <Text fontSize="14px">
+                Would you rather is a React and Redux powered web app built to
+                mimic would you rather game? built this project with React,
+                Redux, Redux-toolkit, React-strap, and Bootstrap for styling.
               </Text>
               <Divider />
             </Text>
           </TwoColumnGrid>
           <TwoColumnGrid>
-            <Heading as="h2" fontSize="xl">
+            <Heading as="h2" fontSize="xl" textAlign="center">
               Education
             </Heading>
             <Text>
@@ -270,44 +240,28 @@ export default function ResumePage() {
                 <Heading as="h3" fontSize="lg">
                   BS, Computer Science and Statistics.
                 </Heading>
-                Alexandria University.
-                <Text fontSize="sm">September 2017 – July 2022</Text>
+                <Text fontSize="14px">Alexandria University. (2022)</Text>
                 <Divider />
               </Text>
               <Text>
                 <Heading as="h3" fontSize="lg">
                   Web development specialization
                 </Heading>
-                University of Michigan via Coursera
-                <Text as="ol" ml="1rem">
-                  <li>Introduction to HTML5.</li>
-                  <li>Introduction to CSS3.</li>
-                  <li>Interactivity with JavaScript.</li>
-                  <li>Advanced Styling with Responsive Design.</li>
-                  <li>Web development specialization capstone </li>
+                <Text fontSize="14px">
+                  University of Michigan via Coursera (2019)
                 </Text>
-              </Text>
-              <Text>
-                <Heading as="h3" fontSize="lg">
-                  Getting Started with Python
-                </Heading>
-                University of Michigan via Coursera
               </Text>
             </Text>
           </TwoColumnGrid>
           <TwoColumnGrid>
-            <Heading as="h2" fontSize="xl" id="skills">
+            <Heading as="h2" fontSize="xl" id="skills" textAlign="center">
               Skills
             </Heading>
-            <Text as="ol" ml="1rem">
-              <li>Typescript & Javascript</li>
-              <li>React js</li>
-              <li>Redux</li>
-              <li>HTML & CSS</li>
-              <li>Git & Github</li>
-              <li>CSS-in-Js & SASS</li>
-              <li>Prototyping with framer</li>
-              <li>Googling</li>
+            <Text as="ol" ml="1rem" fontSize="14px">
+              Nextjs & Gatsby js • Typescript & Javascript • Redux& Redux
+              toolkit • HTML & CSS • Git& GitHub • CSS-in-Js& sass • NodeJs -
+              Express - Adonisjs • Prototyping with Framer • Graphic design &
+              Photo editing • Googling !
             </Text>
           </TwoColumnGrid>
         </VStack>
