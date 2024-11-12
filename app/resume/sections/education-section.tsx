f9fcc134c13af9352f8e5503a8c739595fdfe80c
jan <jankarm2020@gmail.com>
2024-11-12 14:35:25 +0200

    improve mobile layout

--- app/resume/sections/education-section.tsx
+++ app/resume/sections/education-section.tsx
@@ -6,43 +6,39 @@ import { AiTwotoneCalendar } from "react-icons/ai";
 import { MdLocationOn } from "react-icons/md";
 import LineSeparator from "../components/LineSeparator";
 
+const Education = [
+  {
+    name: "BS, Computer Science and Statistics",
+    time: "2017 - 2022",
+    place: "Alexandria University",
+  },
+  {
+    name: "Web development Specialization",
+    time: "2019 - 2021",
+    place: "University of Michigan via Coursera",
+  },
+];
+
 const EducationSection = () => (
   <FullWidthColumn>
     <SectionHeading>Education</SectionHeading>
     <Section className="flex flex-col gap-0.5">
-      <div>
-        <h3 className={"text-lg print:text-base"}>
-          BS, Computer Science and Statistics{" "}
-        </h3>
-        <div className={"text-sm flex flex-row gap-2"}>
-          <div className={"flex flex-row items-center"}>
-            <AiTwotoneCalendar className={"inline"} />
-            &nbsp;2017 - 2022
+      {Education.map(({ name, time, place }) => (
+        <div className={"group"} key={name}>
+          <h3 className={"text-lg print:text-base"}>{name}</h3>
+          <div className={"text-sm flex flex-row flex-wrap gap-x-2"}>
+            <div className={"flex flex-row items-center"}>
+              <AiTwotoneCalendar className={"inline"} />
+              &nbsp;{time}
+            </div>
+            <p className={"flex flex-row items-center"}>
+              <MdLocationOn className={"inline"} />
+              <span className={"text-sky-500"}>{place}</span>
+            </p>
           </div>
-          <p className={"flex flex-row items-center"}>
-            <MdLocationOn className={"inline"} />
-            <span className={"text-sky-500"}>Alexandria University.</span>
-          </p>
+          <LineSeparator />
         </div>
-      </div>
-      <LineSeparator />
-      <div>
-        <h3 className={"text-lg print:text-base"}>
-          Web development Specialization
-        </h3>
-        <div className={"text-sm flex flex-row gap-2"}>
-          <div className={"flex flex-row items-center"}>
-            <AiTwotoneCalendar className={"inline"} />
-            &nbsp;2019 - 2021
-          </div>
-          <p className={"flex flex-row items-center"}>
-            <MdLocationOn className={"inline"} />
-            <span className={"text-sky-500"}>
-              University of Michigan via Coursera.
-            </span>
-          </p>
-        </div>
-      </div>
+      ))}
     </Section>
   </FullWidthColumn>
 );
--- app/resume/sections/experience-section.tsx
+++ app/resume/sections/experience-section.tsx
@@ -20,12 +20,15 @@ const Job = ({ title, company, time, points, location }: JobProps) => {
     <div className={"flex flex-col gap-0.2 group"}>
       <h4 className={`text-lg print:text-base capitalize`}>{title}</h4>
       <h5 className="text-sm leading-none font-bold text-sky-500">{company}</h5>
-      <p className="text-sm print:inline-flex flex items-center">
-        <AiTwotoneCalendar className={"inline"} />
-        &nbsp;{time}
-        &nbsp;&nbsp;
-        <MdLocationOn className={"inline"} />
-        &nbsp;{location}
+      <p className="text-sm print:inline-flex flex flex-wrap items-center gap-x-2">
+        <span className={"flex items-center"}>
+          <AiTwotoneCalendar className={"inline"} />
+          &nbsp;{time}
+        </span>
+        <span className={"flex items-center"}>
+          <MdLocationOn className={"inline"} />
+          &nbsp;{location}
+        </span>
       </p>
       {Array.isArray(points) ? (
         <ol className="text-sm mb-1">
