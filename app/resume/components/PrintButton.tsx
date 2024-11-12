0e4242a0bde8d4c43cadc35ec744897b61362b17
jan <jankarm2020@gmail.com>
2024-11-12 05:14:17 +0200

    remove OR

--- app/resume/components/PrintButton.tsx
+++ app/resume/components/PrintButton.tsx
@@ -13,7 +13,8 @@ const PrintButton = () => {
       >
         PDF
       </button>
-      |{/*<button*/}
+      {/*|*/}
+      {/*<button*/}
       {/*  className="text-2xl hover:underline inline-flex appearance-none items-center justify-center relative whitespace-nowrap align-baseline outline-none outline-offset-2 w-auto leading-normal rounded-md font-bold transition-all duration-100 h-auto min-w-10 text-sky-500 text-center p-0"*/}
       {/*  onClick={() => {*/}
       {/*    Export2Word(resumeId, "Resume_Jan_Ghaly");*/}
