09016959d4a20bf27ace12c78ea805b41d743584
jan <jankarm2020@gmail.com>
2024-11-24 20:22:04 +0200

    maybe fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 import UnoCSS from "@unocss/webpack";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
