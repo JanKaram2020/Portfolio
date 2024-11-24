f2b1fac902d899418d058c4767412993ef7f961c
jan <jankarm2020@gmail.com>
2024-11-24 11:45:21 +0200

    improve resume

--- app/resume/sections/print-section.tsx
+++ app/resume/sections/print-section.tsx
@@ -6,8 +6,8 @@ const PrintSection = () => {
     <div className="flex items-center justify-center mt-[5vh] print:(m-0 hidden) gap-2">
       <a
         className="text-2xl hover:underline inline-flex appearance-none items-center justify-center relative whitespace-nowrap align-baseline outline-none outline-offset-2 w-auto leading-normal rounded-md font-bold transition-all duration-100 h-auto min-w-10 text-sky-500 text-center p-0"
-        href={"/Jan_Ghaly_Resume.pdf"}
-        download={"Jan_Ghaly_Resume.pdf"}
+        href={"/Resume_Jan_Ghaly.pdf"}
+        download={"Resume_Jan_Ghaly.pdf"}
       >
         Download Resume
       </a>
