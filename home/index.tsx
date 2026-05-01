46d05a1c5c613d9c4913831066c85a5b1996d549
Jan Karam <65977048+JanKaram2020@users.noreply.github.com>
2026-05-01 20:33:24 +0300

    embed mozio

--- home/index.tsx
+++ home/index.tsx
@@ -19,6 +19,21 @@ export default function IndexPage(): React.ReactElement {
         <SkillsSection />
         <ProjectsSection />
         <ContactSection />
+        <div className={"flex flex-col items-center justify-center gap-6"}>
+          <iframe
+            className={"mt-16"}
+            src="https://front-end-staging.mozio.com/"
+            title="Mozio"
+          ></iframe>
+          <iframe
+            src="https://front-end-staging.mozio.com/v2/en-us/search?start_name=CDG%3A%20Paris%2C%20FR%20-%20Charles%20De%20Gaulle&end_name=ORY%3A%20Paris%2C%20FR%20-%20Paris%20Orly&start_address=CDG%3A%20Paris%2C%20FR%20-%20Charles%20De%20Gaulle&end_address=ORY%3A%20Paris%2C%20FR%20-%20Paris%20Orly&pickup_datetime=05%2F30%2F2026%2012%3A00%20AM&mode=one_way&num_passengers=1&currency=USD&return_pickup_datetime=12%2F09%2F2025%2005%3A58%20PM"
+            title="Mozio"
+          ></iframe>
+          <iframe
+            src="https://front-end-staging.mozio.com/en-us/search?start_name=CDG%3A+Paris%2C+FR+-+Charles+De+Gaulle&end_name=ORY%3A+Paris%2C+FR+-+Paris+Orly&start_address=CDG&end_address=ORY&pickup_datetime=05%2F30%2F2026+12%3A00+AM&mode=one_way&num_passengers=1&currency=USD&return_pickup_datetime=06%2F08%2F2026+12%3A00+AM&ta=1"
+            title="Mozio"
+          ></iframe>
+        </div>
       </main>
     </>
   );
