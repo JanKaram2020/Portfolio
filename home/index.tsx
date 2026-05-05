269f632b3b9e858355db824f3edb10c8d1bbbabb
jan <jankaram2020@gmail.com>
2026-05-05 16:04:48 +0300

    remove mozio embeds

--- home/index.tsx
+++ home/index.tsx
@@ -19,18 +19,6 @@ export default function IndexPage(): React.ReactElement {
         <SkillsSection />
         <ProjectsSection />
         <ContactSection />
-        <div className={"flex flex-col items-center justify-center gap-6"}>
-          <iframe
-            className={"mt-16 w-full min-h-[100vh]"}
-            src="https://mozio.com/"
-            title="Mozio"
-          ></iframe>
-          <iframe
-            className={"mt-16 w-full min-h-[100vh]"}
-            src="https://www.mozio.com/v2/en-us/search?start_name=CDG%3A%20Paris%2C%20FR%20-%20Paris%20Charles%20De%20Gaulle%20Airport&end_name=ORY%3A%20Paris%2C%20FR%20-%20Paris%20Orly%20Airport&start_address=CDG&end_address=ORY&pickup_datetime=05%2F31%2F2026%2003%3A51%20PM&mode=one_way&num_passengers=1&currency=USD&return_pickup_datetime=06%2F03%2F2026%2003%3A51%20PM"
-            title="Mozio"
-          ></iframe>
-        </div>
       </main>
     </>
   );
